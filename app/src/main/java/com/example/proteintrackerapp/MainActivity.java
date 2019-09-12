05f37a548d61854b0797fcd4ca976e352f3b3176
laurentiayc <laurentia.yulia@si.ukdw.ac.id>
2019-09-12 12:42:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/proteintrackerapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.proteintrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
